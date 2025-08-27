b07aa745dfca8b6d793f4de963f03242d543493d
j.toirjonov <j.toirjonov@ung.uz>
2025-08-27 11:47:34 +0500

    finish this widget

---
+++ lib/hidden_drawer.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_widgets/pages/home_page.dart';
+import 'package:flutter_ui_widgets/pages/profile_page.dart';
+import 'package:flutter_ui_widgets/pages/settings_page.dart';
+import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
+
+class MyHiddenDrawer extends StatefulWidget {
+  const MyHiddenDrawer({super.key});
+
+  @override
+  State<MyHiddenDrawer> createState() => _MyHiddenDrawerState();
+}
+
+class _MyHiddenDrawerState extends State<MyHiddenDrawer> {
+  List<ScreenHiddenDrawer> _pages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name: "Home",
+          baseStyle: TextStyle(),
+          selectedStyle: TextStyle(),
+          colorLineSelected: Colors.indigo,
+      ),
+        MyHomePage(),
+      ),
+
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name: "Profile",
+          baseStyle: TextStyle(),
+          selectedStyle: TextStyle(),
+          colorLineSelected: Colors.indigo,
+        ),
+        MyProfilePage(),
+      ),
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name: "Settings",
+          baseStyle: TextStyle(),
+          selectedStyle: TextStyle(),
+          colorLineSelected: Colors.indigo,
+        ),
+        MySettingsPage(),
+      ),
+
+    ];
+  }
+  @override
+  Widget build(BuildContext context) {
+    return HiddenDrawerMenu(
+        screens: _pages,
+        backgroundColorMenu: Colors.indigo.shade200
+    );
+  }
+}
